7e3c5189932294a0db8d13ef40c0e42833151cb5
kowit <vit_nakon@hotmail.com>
2020-02-22 22:18:43 +0700

    delete file don't use

--- Service/database/seeds/DatabaseSeeder.php
+++ Service/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
          $this->call(PermissionSeeder::class);
          $this->call(RoleSeeder::class);
          $this->call(UserSeeder::class);
-         $this->call(ClientSeeder::class);
     }
 }
