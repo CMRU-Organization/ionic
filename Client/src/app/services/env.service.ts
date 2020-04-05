1e2f500b0395f8bd1088c769a42973a7b96373d3
charlie.cm <vit_nakon@hotmail.com>
2020-04-05 14:28:55 +0700

    fix config

--- Client/src/app/services/env.service.ts
+++ Client/src/app/services/env.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  //API_URL = 'http://www.regapp.cmru.ac.th';
-  API_URL = 'http://localhost:9000/api';
+  API_URL = 'http://www.regapp.cmru.ac.th/api';
+  //API_URL = 'http://localhost:9000/api';
   constructor() { }
 }
